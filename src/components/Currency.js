da8d8ebfc986d1ede8441a4f9701cf2b29a8cc2e
Gino <115049934+MrBluefish@users.noreply.github.com>
2023-03-18 22:03:08 -0400

    Update Currency.js

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,20 +1,26 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 
 const Currency = ({ changeCurrency }) => {
+  const [currency, setCurrency] = useState('£');
+
+  const handleCurrencyChange = (event) => {
+    const selectedCurrency = event.target.value;
+    setCurrency(selectedCurrency);
+    changeCurrency(selectedCurrency);
+  }
+
   return (
-		<div className='alert alert-secondary'>
-        <label style={{marginLeft: '1rem' , backgroundColor:'#33FF49', color:'white'}} >Currency
-      <select name='hover_color'id="currency" onChange={(event)=>changeCurrency(event.target.value)} style={{ marginLeft: '1rem' , backgroundColor:'#33FF49', color:'white'}}>
-        <option style={{color:'black'}} value="£">£ Pound</option>
-        <option style={{color:'black'}} value="$">$ Dollar</option>
-        <option style={{color:'black'}} value="€">€ Euro</option>
-        <option style={{color:'black'}} value="₹">₹ Rupee</option>
-      </select>	
+    <div className='alert alert-secondary'>
+      <label style={{ marginLeft: '1rem', backgroundColor: '#9be0a0', color: 'white' }}>Currency
+        <select name='hover_color' id="currency" onChange={handleCurrencyChange} style={{ marginLeft: '1rem', backgroundColor: '#9be0a0', color: 'white' }}>
+          <option style={{ color: 'black' }} value="£">£ Pound</option>
+          <option style={{ color: 'black' }} value="$">$ Dollar</option>
+          <option style={{ color: 'black' }} value="€">€ Euro</option>
+          <option style={{ color: 'black' }} value="₹">₹ Rupee</option>
+        </select>
       </label>
-      	
     </div>
-	);
+  );
 };
 
 export default Currency;
-
