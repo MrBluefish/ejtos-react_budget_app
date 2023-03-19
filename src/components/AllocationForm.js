80a60c011ce1918e0ff449a2fa8b825fec377704
Gino <115049934+MrBluefish@users.noreply.github.com>
2023-03-18 21:59:56 -0400

    Update AllocationForm.js
    currency prefix

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -59,14 +59,16 @@ const AllocationForm = (props) => {
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
 
-                    <input
-                        required='required'
-                        type='number'
-                        id='cost'
-                        value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
-                        onChange={(event) => setCost(event.target.value)}>
-                        </input>
+                    <span style={{ marginLeft: '1.5rem' }}>£</span>
+
+                  <input
+                    required='required'
+                    type='number'
+                    id='cost'
+                    value={cost}
+                    onChange={(event) => setCost(event.target.value)}
+                    style={{ marginLeft: '0.3rem', width: '10rem' }}/>
+                    
 
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
                         Save
@@ -79,4 +81,5 @@ const AllocationForm = (props) => {
 };
 
 export default AllocationForm;
+
 
